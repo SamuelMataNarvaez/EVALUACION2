bb04001fff2fcbda921ed2e43e61c29811e1579f
SamuelMataNarvaez <159494333+SamuelMataNarvaez@users.noreply.github.com>
2024-04-10 15:35:15 -0600

    Add files via upload

---
+++ EVA_18_FOR/src/eva_18_for/EVA_18_FOR.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_18_for;
+
+/**
+ *
+ * @author user
+ */
+public class EVA_18_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //INICIO; CONDICION; INCREMENTO/DECREMENTO
+        for(int i = 1; i<= 10; i ++) {
+            //LLAVES SON OPCIONALES,USAR CUANDO TENGAN MAS DE UNA INSTRUCCION.
+            System.out.print(i + "-");
+            
+        
+    }
+        System.out.println("");
+        for(int i = 10; i>= 1; i --) {
+            //LLAVES SON OPCIONALES,USAR CUANDO TENGAN MAS DE UNA INSTRUCCION.
+            System.out.print(i + "-");
+        }
+            
+        //IMPRIMIR TODOS LOS NUMEROS PARES ENTRE 0 Y 100
+        //0,2,4,6,8,10
+            System.out.println("");
+        for(int i = 0; i <= 100; i += 2){
+        System.out.print(i + "-");
+       
+    }
+        
+        }
+        
+    }
+      
