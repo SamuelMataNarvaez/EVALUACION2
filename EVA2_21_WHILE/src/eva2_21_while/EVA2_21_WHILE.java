14c170b9d1382294685e32744c6dd384ea9e749b
SamuelMataNarvaez <159494333+SamuelMataNarvaez@users.noreply.github.com>
2024-04-16 16:03:07 -0600

    Add files via upload

---
+++ EVA2_21_WHILE/src/eva2_21_while/EVA2_21_WHILE.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_21_while;
+
+/**
+ *
+ * @author user
+ */
+public class EVA2_21_WHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //FOR --> cuenta.
+        //while y do while --> con condiciones.
+        int cont = 11;
+        while(cont <= 10){
+            System.out.print(cont + " - ");
+            cont++;
+        }
+    }
+    
+}
